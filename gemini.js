a265848d74efdd414835d638bf224137041c9754
Manubhav-Chauhan <167097089+Manubhav-Chauhan@users.noreply.github.com>
2026-06-30 16:44:51 +0530

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -10,7 +10,6 @@ const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
 });
-
 const generationConfig = {
   temperature: 1,
   topP: 0.95,
